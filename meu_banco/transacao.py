42033a5f1a5960cbb286fdf511d9b39c29f2d377
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-09-01 17:48:52 -0300

    Bootcamp NTTData: Desafio 3, Implementando a classe transacao.

---
+++ meu_banco/transacao.py
@@ -0,0 +1,38 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class Transacao(ABC):
+    @abstractmethod
+    def registrar(self, conta):
+        pass
+
+
+class Deposito(Transacao):
+    def __init__(self, valor: float):
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    def registrar(self, conta):
+        if conta.depositar(self.__valor):
+            conta.historico.adicionar_transacao(
+                f'Depósito de R${self.__valor:.2f}'
+            )
+
+
+class Saque(Transacao):
+    def __init__(self, valor: float):
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    def registrar(self, conta):
+        if conta.sacar(self.__valor):
+            conta.historico.adinionar_transacao(
+                f'Saque de R${self.__valor:.2f}'
+            )
